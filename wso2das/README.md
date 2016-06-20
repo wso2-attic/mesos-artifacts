a254c4281152166764fddba1af61309e735395bd
Pubudu Gunatilaka <pubudu538@gmail.com>
2016-06-20 11:49:30 +0530

    Adding wso2das mesos artifacts

---
+++ wso2das/README.md
@@ -0,0 +1,16 @@
+# Mesos Artifacts for WSO2 Data Analytics Server
+
+These Mesos Artifacts provide the resources and instructions to deploy WSO2 Data Analytics Server on Mesos DC/OS.
+
+## Getting Started
+
+To deploy a WSO2 product on Mesos DC/OS, follow the below steps:
+
+* Build WSO2 Data Analytics Server Docker images using [WSO2 Dockerfiles](https://github.com/wso2/dockerfiles).
+* Load above Docker images to Mesos slave nodes or import them to a central Docker registry.
+* If a central Docker registry is used, update Docker image tags accordingly in WSO2 Data Analytics Server Marathon applications.
+* Run `deploy.sh` found inside this folder. It will deploy following containers:
+   * Marathon load balancer container
+   * WSO2 Governance Registry database container
+   * WSO2 User Management database container
+   * WSO2 Data Analytics Server Configuration Registry database container
---
+++ wso2das/deploy.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+#
+# Copyright 2016 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+# ------------------------------------------------------------------------
+
+set -e
+self_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+marathon_endpoint="http://m1.dcos:8080/v2"
+source "${self_path}/../common/scripts/base.sh"
+
+bash ${self_path}/../common/marathon-lb/deploy.sh
+echo "Waiting for marathon-lb to launch on a1.dcos:9090..."
+while ! nc -z a1.dcos 9090; do
+  sleep 0.1
+done
+echo "marathon-lb started successfully"
+
+bash ${self_path}/../common/wso2-shared-dbs/deploy.sh
+deploy ${marathon_endpoint} ${self_path}/mysql-das-db.json
+
+echo "Waiting for mysql-gov-db to launch on a1.dcos:10000..."
+while ! nc -z a1.dcos 10000; do
+  sleep 0.1
+done
+echo "mysql-gov-db started successfully"
+
+echo "Waiting for mysql-user-db to launch on a1.dcos:10001..."
+while ! nc -z a1.dcos 10001; do
+  sleep 0.1
+done
+echo "mysql-user-db started successfully"
+
+echo "Waiting for mysql-das-db to launch on a1.dcos:10002..."
+while ! nc -z a1.dcos 10002; do
+  sleep 0.1
+done
+echo "mysql-das-db started successfully"
+
+deploy ${marathon_endpoint} ${self_path}/wso2das-default.json
+echo "Waiting for wso2das-default to launch on a1.dcos:10092..."
+while ! nc -z a1.dcos 10092; do
+ sleep 0.1
+done
+echo "wso2das-default started successfully: https://wso2das-default:10092/carbon"
+
