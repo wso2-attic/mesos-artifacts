4201b4f123ca6bb991579e89239de249377b4407
Isuru Haththotuwa <isuruh@apache.org>
2016-07-21 19:52:15 +0530

    wso2das-default with HOST n/w mode

--- wso2das/deploy.sh
+++ wso2das/deploy.sh
@@ -22,15 +22,20 @@ self_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 mesos_artifacts_home="${self_path}/.."
 source "${mesos_artifacts_home}/common/scripts/base.sh"
 
+wso2_das_marathon_app_id=wso2das-default
 mysql_das_db_host_port=10061
-wso2das_default_service_port=10063
+wso2das_default_service_port=9763
+wso2das_thrift_port=7611
+wso2das_secure_thrift_port=7711
 
 function deploy_default() {
   echoBold "Deploying WSO2 DAS default setup on Mesos..."
   deploy_common_services
   deploy_service 'mysql-das-db' $mysql_das_db_host_port
   deploy_service 'wso2das-default' $wso2das_default_service_port
-  echoBold "wso2das-default management console: https://${marathonlb_host_ip}:${wso2das_default_service_port}/carbon"
+  echoBold "wso2das-default management console: http://${marathonlb_host_ip}:${wso2das_default_service_port}/carbon"
+  echoBold "wso2das-default thrift endpoint: http://${wso2_das_marathon_app_id}.marathon.mesos:${wso2das_thrift_port}"
+  echoBold "wso2das-default thrift secure endpoint: https://${wso2_das_marathon_app_id}.marathon.mesos:${wso2das_secure_thrift_port}"
   echoSuccess "Successfully deployed WSO2 DAS default setup on Mesos"
 }
 
